6c1b22add6a1a7e48e3555d40f7b3c48826a4c97
Kavyachittineni <107785319+Kavyachittineni@users.noreply.github.com>
2022-06-30 20:40:46 +0530

    Perimeterofrectangle

---
+++ Perimeterofrectangle.java
@@ -0,0 +1,26 @@
+
+
+public class Perimeterofrectangle {
+
+	int length;
+	int width;
+	void insert(int l, int w) {
+		length = l;
+		width = w;
+		
+	}
+	void calculatePerimeter() {
+		System.out.println(2*(length+width));
+		}
+	class Rectanle {
+		
+	
+	public static void main(String[] args) {
+	
+		Perimeterofrectangle r1 = new Perimeterofrectangle();
+		r1.insert(10,50);
+		r1.calculatePerimeter();
+	}
+	
+	}
+}
