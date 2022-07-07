e3cefe152e94b104cdc81f022898ceed5a4e496f
Kavyachittineni <107785319+Kavyachittineni@users.noreply.github.com>
2022-07-07 09:21:09 +0530

    inherit

---
+++ inherit.java
@@ -0,0 +1,16 @@
+class base{
+    int id = 101;
+}
+class derived extends base{
+    int salary = 60000;
+}
+
+public class inherit {
+    public static void main(String args[]){
+        System.out.println("This is just simple inheritance");
+        derived i = new derived();
+
+        System.out.println("Your  id is "+ i.id);
+        System.out.println("Your salary is "+ i.salary);
+    }
+}
