d1ec080e64131d57e61562c742105f1d57cc9975
Kavyachittineni <107785319+Kavyachittineni@users.noreply.github.com>
2022-07-10 10:24:04 +0530

    Overloading

---
+++ OverLoading.java
@@ -0,0 +1,22 @@
+public class OverLoading {
+	static int add(int a, int b) 
+	{
+		return (a+b);
+	}
+
+		static int add1(int a, int b, int c)
+		{
+			return (a+b+c);
+		}
+		class overloading{
+			public static void main(String[] args) {
+				System.out.println(OverLoading.add(10,10 ));
+				System.out.println(OverLoading.add1(10,10,10));
+			}
+		
+	}
+	
+	
+
+}
+
